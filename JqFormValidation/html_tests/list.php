c0fceeadcc5e773a4aeb01e1f9aa6d6fd9311eef
Clément <maboiteaspam@wanadoo.fr>
2011-08-16 18:46:51 +0200

    Add a new HTML test concerning named list (<input type="text" name="test[]" />)

---
+++ JqFormValidation/html_tests/list.php
@@ -0,0 +1,43 @@
+<?php
+
+
+include("../libs/__include_files.php");
+
+use BricEtBroc\Form\FormValidator as FormValidator;
+use BricEtBroc\Form\InputValueAccessor as InputValueAccessor;
+use BricEtBroc\Form\Dependency as Dependency;
+
+$options = array('rules' => array(
+                    'testfield[]' => 'required',
+                    ),
+                );
+$validator = new FormValidator("f_testform", $options);
+
+if( $_SERVER['REQUEST_METHOD'] === "POST" ){
+    $validator->setDataSource($_POST);
+    $validator->validate();
+    if( $validator->hasErrors() === false ){
+        echo "ok";
+    }
+}
+
+?>
+<html>
+    <head>
+        <script type="text/javascript" src="../vendors/jquery-validation/lib/jquery-1.6.1.js"></script>
+        <script type="text/javascript" src="../vendors/jquery-validation/lib/jquery.form.js"></script>
+        <script type="text/javascript" src="../vendors/jquery-validation/jquery.validate.js"></script>
+    </head>
+    <body>
+        <form name="f_testform" method="POST" action="">
+            <label for="testfield">Input field</label>
+            <input type="text" name="testfield[]" id="testfield3" value="" />
+            <br/>
+            <label for="testfield2">Input field 2</label>
+            <input type="text" name="testfield[]" id="testfield2" value="" />
+            <br/>
+            <input type="submit" value="Submit !" />
+        </form>
+        <?php echo $validator->__toHTML(); ?>
+    </body>
+</html>
