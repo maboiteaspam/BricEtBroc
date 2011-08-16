f7b8b487c06817c00e239780e5ffa1eab6325939
Clément <maboiteaspam@wanadoo.fr>
2011-08-16 18:50:17 +0200

    Added a new prop to tell if a datasource is textual or not

--- JqFormValidation/libs/InputValues.php
+++ JqFormValidation/libs/InputValues.php
@@ -7,22 +7,56 @@ class InputValues{
     public $data_sources;
     const DFT_SRCE = "from_default";
     
-    public function __construct( array $values, $data_source_name=InputValues::DFT_SRCE ){
+    /**
+     *
+     * @param array $values
+     * @param string $data_source_name 
+     * @param bool $isTextual 
+     */
+    public function __construct( array $values, $data_source_name=InputValues::DFT_SRCE, $isTextual=true ){
         $this->data_sources = array();
-        $this->setDataSource( $values, $data_source_name );
+        $this->setDataSource( $values, $data_source_name, $isTextual );
     }
     
-    public function setDataSource( array $values, $data_source_name=InputValues::DFT_SRCE ){
-        $this->data_sources[$data_source_name] = $values;
+    /**
+     *
+     * @param array $values
+     * @param string $data_source_name 
+     * @param bool $isTextual 
+     */
+    public function setDataSource( array $values, $data_source_name=InputValues::DFT_SRCE, $isTextual=true ){
+        $this->data_sources[$data_source_name] = array(
+            "content"=>$values,
+            "is_textual"=>$isTextual,
+        );
     }
     
+    /**
+     *
+     * @param string $date_source_name
+     * @return mixed 
+     */
     public function getDataSource( $date_source_name=InputValues::DFT_SRCE ){
-        return $this->data_sources[$date_source_name];
+        return $this->data_sources[$date_source_name]["content"];
     }
     
+    /**
+     *
+     * @param string $date_source_name
+     * @return mixed 
+     */
+    public function isTextual( $date_source_name=InputValues::DFT_SRCE ){
+        return $this->data_sources[$date_source_name]["is_textual"];
+    }
+    
+    /**
+     *
+     * @param string $target
+     * @return InputValueAccessor 
+     */
     public function getAccessor( $target ){
         $retour = new InputValueAccessor( $target );
-        $retour->setDataSource($this);
+        $retour->setInputValues($this);
         return $retour;
     }
 }
