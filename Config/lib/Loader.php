0274cf5d2ad82213a00a2aae5e1d91422aff5c08
Clément <maboiteaspam@wanadoo.fr>
2011-08-23 23:28:12 +0200

    Added container prop and some minors updates

--- Config/lib/Loader.php
+++ Config/lib/Loader.php
@@ -10,21 +10,24 @@ use \BricEtBroc\FilesLoader\ILoader as ILoader;
  * @author clement
  */
 class Loader implements ILoader{
-    protected $path_to_config_dirs;
+    
+    protected $path_to_load_dirs;
     protected $files_to_load;
     protected $builded_lists;
+    protected $container;
+    
     protected $dependant_files;
     
-    public function __construct( $path_to_config_dirs=array(), $files_to_load=array() ){
-        $this->path_to_config_dirs  = $path_to_config_dirs;
-        $this->files_to_load        = $files_to_load;
+    public function __construct( $path_to_load_dirs=array(), $files_to_load=array() ){
+        $this->path_to_load_dirs  = $path_to_load_dirs;
+        $this->files_to_load      = $files_to_load;
         $this->reset();
     }
     
     public function reset(){
-        $this->unique_name          = null;
         $this->builded_lists        = array();
         $this->dependant_files      = array();
+        $this->container            = null;
     }
     
     public function addFileToLoad( $file_to_load, $at_the_end=true ){
@@ -39,13 +42,13 @@ class Loader implements ILoader{
         return false;
     }
     
-    public function addPathToLoadDir( $path_to_config_dir, $at_the_end=true ){
-        if( in_array($path_to_config_dir, $this->path_to_config_dirs) === false ){
+    public function addPathToLoadDir( $path_to_load_dir, $at_the_end=true ){
+        if( in_array($path_to_load_dir, $this->path_to_load_dirs) === false ){
             $this->reset();
             if( $at_the_end )
-                $this->path_to_config_dirs[] = $path_to_config_dir;
+                $this->path_to_load_dirs[] = $path_to_load_dir;
             else
-                array_unshift ($this->path_to_config_dirs, $path_to_config_dir);
+                array_unshift ($this->path_to_load_dirs, $path_to_load_dir);
             return true;
         }
         return false;
@@ -63,7 +66,7 @@ class Loader implements ILoader{
     
     public function listOfFiles(){
         if( count($this->builded_lists) == 0 )
-            $this->builded_lists = $this->buildListOfFiles($this->path_to_config_dirs, $this->files_to_load);
+            $this->builded_lists = $this->buildListOfFiles($this->path_to_load_dirs, $this->files_to_load);
         return $this->builded_lists;
     }
     
@@ -95,6 +98,9 @@ class Loader implements ILoader{
     }
     
     public function get(){
-        return $this->createResponse( $this->load() );
+        if( $this->container === null ){
+            $this->container = $this->createResponse( $this->load() );
+        }
+        return $this->container;
     }
 }
