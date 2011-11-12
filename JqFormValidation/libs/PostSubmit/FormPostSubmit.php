99d800882d3d7bb5e5cad66b1aa336872d617f7d
Clément <maboiteaspam@wanadoo.fr>
2011-11-12 11:43:01 +0800

    Updated render method, more simple, more clean

--- JqFormValidation/libs/PostSubmit/FormPostSubmit.php
+++ JqFormValidation/libs/PostSubmit/FormPostSubmit.php
@@ -68,14 +68,20 @@ class FormPostSubmit implements IFormComponent {
     }
     
     public function render( $is_submitted, $has_validated, \DOMDocument $doc ){
-        $xpath      = new \DOMXpath($doc);
+
+        $xpath          = new \DOMXpath($doc);
+        $form_element   = $xpath->query("//form[@name='".$this->targetElement."']");
+        if( $form_element === false || $form_element->length > 0 ){
+            $form_element = null;
+        }else{
+            $form_element = $form_element->item(0);
+        }
         
-        if( isset($this->options["rules"]) ){
-            $elements   = $xpath->query("//form[@name='".$this->targetElement."']");
-            if( $elements !== false && $elements->length > 0 ){
-                if( $is_submitted ){
+        if( $form_element !== null ){
+            if( $is_submitted ){
+                if( isset($this->options["rules"]) ){
                     foreach( $this->options["rules"] as $elementTarget=>$infos ){
-                        $input_el   = $xpath->query("//*[@name='".$elementTarget."']", $elements->item(0) );
+                        $input_el   = $xpath->query("//*[@name='".$elementTarget."']", $form_element );
                         if( $input_el!==false && $input_el->length > 0 ){
                             if( $this->input_values->getAccessor($elementTarget)->is_set() ){
                                 if( $input_el->item(0)->tagName == "input" ){
@@ -87,17 +93,16 @@ class FormPostSubmit implements IFormComponent {
                         }
                     }
                 }
-                
-
-                $hidden_id = $doc->createElement ('input');
-                $hidden_id->setAttribute("type", "hidden");
-                $hidden_id->setAttribute("name", "form_id");
-                $hidden_id->setAttribute("value", $this->getIdentifierValue());
-                // Creating an empty text node forces <script></script>
-                $elements->item(0)->appendChild ($hidden_id);
-
             }
+            $hidden_id = $doc->createElement ('input');
+            $hidden_id->setAttribute("type", "hidden");
+            $hidden_id->setAttribute("name", "form_id");
+            $hidden_id->setAttribute("value", $this->getIdentifierValue());
+            // Creating an empty text node forces <script></script>
+            $form_element->appendChild ($hidden_id);
         }
+        unset($form_element);
+        unset($xpath);
         
         return $doc;
     }
