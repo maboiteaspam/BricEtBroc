b209d0862e65e905388e9d772146bc3172fe8202
Clément <maboiteaspam@wanadoo.fr>
2011-11-30 10:25:59 +0800

    Update test suite

--- ActiveModel/tests/index.php
+++ ActiveModel/tests/index.php
@@ -3,13 +3,21 @@
 $current_dir    = __DIR__;
 $dirs           = array();
 foreach( scandir($current_dir) as $f ){
-    if( in_array($f, array(".","..")) == false 
+    if( in_array($f, array(".","..")) == false
          && is_dir($current_dir."/".$f) ){
         $dirs[] = $f;
     }
 }
 ?>
 
-<?php foreach( $dirs as $dir ){ ?>
-    <a href="<?php echo $dir; ?>/"><?php echo ucfirst(str_replace("_", " ", $dir)); ?></a>
-<?php } ?>
+<html>
+<head></head>
+<body style="margin: 0;">
+    <div style="float: left; width:100px; padding: 5px;">
+        <?php foreach( $dirs as $dir ){ ?>
+            <a href="<?php echo $dir; ?>/" target="results"><?php echo ucfirst(str_replace("_", " ", $dir)); ?></a><br/>
+        <?php } ?>
+    </div>
+    <iframe name="results" style="float: left; width: 1050px; height: 100%; border-width: 0px; border-style: solid; border-left-width: 1px;"></iframe>
+</body>
+</html>
